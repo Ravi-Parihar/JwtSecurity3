a8d2e68b3cff909d10d5d7162c14ffc1361a4d1c
rparihar <rparihar.adqworks@gmail.com>
2025-05-22 21:35:04 +0530

    add jwt dependency & api

--- src/main/java/com/metflix/JwtSecurity3/config/AppConfig.java
+++ src/main/java/com/metflix/JwtSecurity3/config/AppConfig.java
@@ -1,2 +1,26 @@
-package com.metflix.JwtSecurity3.config;public class AppConfig {
+package com.metflix.JwtSecurity3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.builder().username("ravi").password(passwordEncoder().encode("abc")).roles("ADMIN").build();
+        UserDetails user1 = User.builder().username("sonu").password(passwordEncoder().encode("abc")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, user1);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/metflix/JwtSecurity3/security/JwtAuthenticationFilter.java
+++ src/main/java/com/metflix/JwtSecurity3/security/JwtAuthenticationFilter.java
@@ -1,2 +1,16 @@
-package com.metflix.JwtSecurity3.security;public class JwtAuthenticationFilter {
+package com.metflix.JwtSecurity3.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
