6a2a59adf45200cf044f497cf85dba8726babbad
rparihar <rparihar.adqworks@gmail.com>
2025-05-27 17:30:53 +0530

    final update jwt auth

--- src/main/java/com/metflix/JwtSecurity3/security/JwtHelper.java
+++ src/main/java/com/metflix/JwtSecurity3/security/JwtHelper.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
 public class JwtHelper {
 
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
-    private String secret = "aertytytycacagafagaafafafafafasfasfasdagdahfahgcvbnSDFGHJKRTYUJCCVxcvbnmkjkuijb";
+    private String secret = "afafasfafafasfasfasfafacasdasfasxASFACASDFACASDFASFASFDAFASFASDAADSCSDFADCVSGCFVADXCcadwavfsfarvf";
 
     /** Retrieve username from jwt token */
     public String getUsernameFromToken(String token){
